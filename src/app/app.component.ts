ee5784b20b6f310191b6d9f4948dcc90626be1fa
swm <seahwm01@gmail.com>
2024-06-08 22:13:46 +0800

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, SecurityContext } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Learn_Angular';
+  servers:string[];
+  title:string|null;
+
+  constructor(
+    protected sanitizer:DomSanitizer
+  ){
+    var a="    '); and i=i+1 <alert>'asd'</alert>";
+    this.title=a;
+    this.title=sanitizer.sanitize(SecurityContext.HTML,a)
+    this.servers=['S1','S2','D1'];
+  }
+
+  sanitize(e:KeyboardEvent){
+    this.title=this.sanitizer.sanitize(SecurityContext.HTML,this.title);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { FormsModule } from '@angular/forms';
+import { Route, RouterModule, Routes } from '@angular/router';
+
+const router :Routes=[
+  {path:"as",component:ServerComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule,RouterModule.forRoot(router)
   ],
   providers: [],
   bootstrap: [AppComponent]
