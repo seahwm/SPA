24aba61ab631a0e1bde5e5aff8eabcefabb2dd38
swm <seahwm01@gmail.com>
2024-06-10 16:17:47 +0800

    Basic route

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-server',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './server.component.html',
+  styleUrl: './server.component.css'
+})
+export class ServerComponent {
+
+}
